309178f51ca81d08bba595676c93f0fb3b0d0133
h4rvey r0ss <h4rvey@protonmail.com>
2017-10-26 11:33:35 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-var router = express.Router({mergeParams: true});
-router.use(bodyParser.json());
+const router = express.Router({mergeParams: true});
 
-router.route('/').get((req, res) => {
-  res.send('Get all users');
-}).post((req, res) => {
-  res.send('Create new user');
-});
+router.route('/')
+  .get((req, res) => {
+    res.send('Get all users');
+  })
+  .post((req, res) => {
+    res.send('Create new user');
+  });
 
-router.route('/:username').get((req, res) => {
-  console.log(req.params);
-  res.send("Get a user's all infos");
-}).put((req, res) => {
-  res.send("Update a user's all infos");
-}).delete((req, res) => {
-  res.send('Delete user');
-});
+router.route('/:username')
+  .get((req, res) => {
+    console.log(req.params);
+    res.send("Get a user's all infos");
+  })
+  .put((req, res) => {
+    res.send("Update a user's all infos");
+  })
+  .delete((req, res) => {
+    res.send('Delete user');
+  });
 
-router.route('/:username/pic').get((req, res) => {
-  res.send("Get a user's pic");
-}).put((req, res) => {
-  res.send('Update user of club');
-});
+router.route('/:username/pic')
+  .get((req, res) => {
+    res.send("Get a user's pic");
+  })
+  .put((req, res) => {
+    res.send('Update user of club');
+  });
 
 module.exports = router;
