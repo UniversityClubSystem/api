16d830998fe7dc64d61f7bc87fb519feabaa846d
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-10-26 09:17:14 +0300

    travis eslint configs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var router = express.Router({
-    mergeParams: true
+  mergeParams: true
 });
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    res.send('Api running!');
+  res.send('Api running!');
 });
 
 module.exports = router;
